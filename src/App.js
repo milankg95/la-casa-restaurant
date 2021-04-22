3228c7d023284d83d67b933b0f464faef7e7e783
milankg95 <milanstojanovicdm@gmail.com>
2021-04-22 21:42:24 +0200

    App changed.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          La Casa restaurant
+          La Casa restaurant!
         </a>
       </header>
     </div>
